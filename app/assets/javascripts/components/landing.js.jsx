db00ab7597af89f2486d5953548076cd4ac1f1d6
Michael A. Holmes <holmes.m.andrew@gmail.com>
2018-07-05 18:11:35 -0500

    first commit

---
+++ app/assets/javascripts/components/landing.js.jsx
@@ -0,0 +1,22 @@
+
+
+var Landing = React.createClass({
+  
+  
+    render: function() {
+      return (
+       <div className="landing">
+         <div className="container">
+          <div className="jumbotron">
+              <h1>Neighborly</h1>
+              <h3>The place were someone always has your back</h3>
+              <Img src="/assets/hat.jpg" id="logo" width="90%" height="90%" />
+          </div>
+        </div>
+       </div>
+          
+      );
+    }
+  });
+  
+  
---
+++ app/assets/javascripts/components/navigation.js.jsx
@@ -0,0 +1,31 @@
+
+var Navigation = React.createClass({
+  
+  
+    render: function() {
+      return (
+       <div className="navigation">
+         <div className="container">
+          
+            <div className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+                    <a className="navbar-brand" href="#"><h4>Neighborly</h4></a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-item nav-link active" href="/"><h6>Home</h6></a>
+                            <a className="nav-item nav-link" href="/login"><h6>Sign In</h6></a>
+                            <a className="nav-item nav-link" href="/users/new"><h6>Sign Up</h6></a>
+                        </div>
+                    </div>
+            </div>
+          
+         </div>
+       </div>
+          
+      );
+    }
+  });
+  
+
