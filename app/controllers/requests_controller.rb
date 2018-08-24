b868b8d47059c377921c477df1cf35e07488d3ce
Michael A. Holmes <holmes.m.andrew@gmail.com>
2018-08-23 20:46:07 -0500

    finishing requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,20 +2,47 @@ class RequestsController < ApplicationController
     before_action :authenticate_user!
 
     def index
+        requests = Request.all
+        render json: {requests: requests}
+    end
+
+    def create
+        request = Request.new(request_params)
+        request.user = current_user
+        if request.save
+           render json:{
+               message: "Your request was saved!",
+               request: request,
+        }else
+
+         render json: {message: "Sorry, something is wrong here..."}
+        end
     end
 
     def show 
+        request = Request.find(params[:id])
+        render json: {request:request}
     end
 
     def update
     end
 
     def destroy 
+        if current_user @request.user
+            if @request.destroy
+                json_response "Your request was deleted", true, {request: @request}, :ok
+            else
+                json_response "Sorry, could not delete", false, {}, :unprocessable_entity
+            end
+        else
+            json_response "Sorry, you do not have permission to delete this request", false, {}, :unauthorized
+        end
     end
 
     private
 
     def request_params
+        params.require(:request).permit(:title, :description, :address, :request_type)
     end
     
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,8 +1,13 @@
 class Request < ApplicationRecord
     belongs_to :user
 
+    validates_presense_of :title, :description, :latitude, :longitude, :user_id, :request_type
     validates :title, length { maximum: 50 }
     validates :description, length { maximum: 300 }
+    valitates_inclusion_of :request_type, { in: allowed_types,
+        message: "The request needs to be Material or Help"}
+    # geocoded_by :address, :latitude => :lat, :longitude => :lng
+    
     
 
 end
