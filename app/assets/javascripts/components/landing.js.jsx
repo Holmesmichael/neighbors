665c461d360cf4e8c09f153c104c9da0e7f38bae
Michael A. Holmes <holmes.m.andrew@gmail.com>
2018-10-11 23:54:37 -0500

    initial styling

--- app/assets/javascripts/components/landing.js.jsx
+++ app/assets/javascripts/components/landing.js.jsx
@@ -7,12 +7,12 @@ var Landing = React.createClass({
       return (
        <div className="landing">
          <div className="container">
-          <div className="jumbotron">
+          
               <h1>Neighborly</h1>
               <h3>The place were someone always has your back</h3>
               <Img src="/assets/hat.jpg" id="logo" width="90%" height="90%" />
               
-          </div>
+          
         </div>
        </div>
           
